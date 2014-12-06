0390778813df9f985106d9c6e7a39a22ad5b12d2
Kayla Thornton <kayperth20@gmail.com>
2014-12-05 21:09:23 -0800

    Additional designs and fixes

--- topflight.js
+++
@@ -1,9 +0,0 @@
-var cpo = []; cpo["_object"] ="cp_widget_3af12a08-bf71-4077-9fbd-6f7f021f1e49"; cpo["_fid"] = "A0MAnJM3OhkI";
-              var _cpmp = _cpmp || []; _cpmp.push(cpo);
-              (function() { var cp = document.createElement("script"); cp.type = "text/javascript";
-              	var s= "http//www.cincopa.com/media-platform/runtime/libasync.js";
-              	console.log('before ' + s);
-              cp.async = true; cp.src = s;
-              	console.log('after ' + cp.src);
-              var c = document.getElementsByTagName("script")[0];
-              c.parentNode.insertBefore(cp, c); })();
